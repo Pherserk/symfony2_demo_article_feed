93d38a2419bbcb87f7b916891f75ad05c21ddccb
Pherserk <michelecarino84@gmail.com>
2016-08-27 05:46:55 +0200

    Add a command to select authors to be notified by email

--- src/AppBundle/Command/NotifyQuotesCommand.php
+++ src/AppBundle/Command/NotifyQuotesCommand.php
@@ -3,7 +3,46 @@
 namespace AppBundle\Command;
 
 
-class NotifyQuotesCommand
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NotifyQuotesCommand extends ContainerAwareCommand
 {
-    #This is a stub
+    protected function configure()
+    {
+        $this
+            ->setName('notify:quotes')
+            ->setDescription('Sends notifications on quotes.')
+            ->setHelp("This command sends email alerts to user having quotes on their articles")
+            ->addOption(
+                'hours',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Hours to look back',
+                24
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $hours = $input->getOption('hours');
+        $from = new \DateTime(sprintf('-%d hours', $hours));
+        $to = new \DateTime('now');
+
+        $output->writeln(sprintf('Looking for quotes made before: %s', $from->format('m/d/Y H:i:s')));
+
+        $quotesRepo = $this->getContainer()
+            ->get('doctrine.orm.default_entity_manager')
+            ->getRepository('AppBundle:Quote');
+
+        $quotes = $quotesRepo->findByCreationBeetween($from, $to);
+
+        foreach ($quotes as $quote) {
+            $output->writeln(sprintf('Sending an email to: %s', $quote->getArticle()->getUser()->getEmail()));
+        }
+
+        $output->writeln('Done');
+    }
 }
---
+++ src/AppBundle/Tests/Command/NotifyQuotesCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Tests\Command;
+
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class NotifyQuotesCommandTest extends WebTestCase {
+
+    public function testExecution()
+    {
+        $referenceRepository = $this
+            ->loadFixtures([
+                'AppBundle\DataFixtures\ORM\LoadUserData',
+                'AppBundle\DataFixtures\ORM\LoadArticleData',
+                'AppBundle\DataFixtures\ORM\LoadQuoteData',
+            ])
+            ->getReferenceRepository();
+
+        $article = $referenceRepository->getReference('article-1');
+
+        $output = $this->runCommand('notify:quotes');
+
+        $outputLines = explode(PHP_EOL, $output);
+
+        $this->assertEquals(
+            sprintf('Sending an email to: %s', $article->getUser()->getEmail()),
+            $outputLines[1]
+        );
+
+        $this->assertEquals(
+            sprintf('Done'),
+            $outputLines[3]
+        );
+    }
+}
