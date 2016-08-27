3602358927f853c41541445e69de6c5d7531e85a
Pherserk <michelecarino84@gmail.com>
2016-08-27 12:06:01 +0200

    Swiftmailer integration

--- src/AppBundle/Command/NotifyQuotesCommand.php
+++ src/AppBundle/Command/NotifyQuotesCommand.php
@@ -27,13 +27,15 @@ class NotifyQuotesCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $container = $this->getContainer();
+
         $hours = $input->getOption('hours');
         $from = new \DateTime(sprintf('-%d hours', $hours));
         $to = new \DateTime('now');
 
         $output->writeln(sprintf('Looking for quotes made before: %s', $from->format('m/d/Y H:i:s')));
 
-        $quotesRepo = $this->getContainer()
+        $quotesRepo = $container
             ->get('doctrine.orm.default_entity_manager')
             ->getRepository('AppBundle:Quote');
 
@@ -41,7 +43,16 @@ class NotifyQuotesCommand extends ContainerAwareCommand
 
         foreach ($quotes as $quote) {
             if ($quote->getAuthor() !== $quote->getArticle()->getUser()) {
+
                 $output->writeln(sprintf('Sending an email to: %s', $quote->getArticle()->getUser()->getEmail()));
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject(sprintf('Quote from: %s', $quote->getAuthor()->getName()))
+                    ->setFrom($container->getParameter('notifications_email'))
+                    ->setTo($quote->getArticle()->getUser()->getEmail())
+                    ->setBody($quote->getText(), 'text/html');
+
+                $container->get('mailer')->send($message);
             }
         }
 
