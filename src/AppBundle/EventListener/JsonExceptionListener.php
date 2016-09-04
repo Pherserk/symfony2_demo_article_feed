eee7502176a51a021ab83a56fe17aa3219745e6c
Pherserk <michelecarino84@gmail.com>
2016-09-04 11:20:40 +0200

    Add an json exception event listner for apis

---
+++ src/AppBundle/EventListener/JsonExceptionListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class JsonExceptionListener
+{
+    private $apiRoutePrefix;
+
+    public function __construct($apiRoutePrefix)
+    {
+        $this->apiRoutePrefix = $apiRoutePrefix;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $route = $event->getRequest()->get('_route');
+        if (strpos($route, $this->apiRoutePrefix) === 0) {
+            $exception = $event->getException();
+            $data = array(
+                'error' => array(
+                    'code' => $exception->getCode(),
+                    'message' => $exception->getMessage()
+                )
+            );
+            $response = new JsonResponse($data);
+            $event->setResponse($response);
+        }
+    }
+}
+
