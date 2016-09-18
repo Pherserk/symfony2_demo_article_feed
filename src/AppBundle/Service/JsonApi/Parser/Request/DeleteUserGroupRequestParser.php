d17910bb2790a6e62ea7e6d7c14152876f034d29
Pherserk <michelecarino84@gmail.com>
2016-09-18 18:05:33 +0200

    user group deletion

---
+++ src/AppBundle/Service/JsonApi/Parser/Request/DeleteUserGroupRequestParser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cagliostro
+ * Date: 18/09/16
+ * Time: 18.03
+ */
+
+namespace AppBundle\Service\JsonApi\Parser\Request;
+
+
+class DeleteUserGroupRequestParser
+{
+
+}
