7761b16ec80f60a80c3c6470719689000ca31b70
Pherserk <michelecarino84@gmail.com>
2016-09-25 21:33:14 +0200

    test placeholders

---
+++ src/AppBundle/Tests/Service/Security/UserEmailVerificatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Tests\Service\Security;
+
+
+class UserEmailVerificatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testVerify()
+    {
+        self::markTestIncomplete('do this test');
+    }
+}
---
+++ src/AppBundle/Tests/Service/Security/UserMobileNumberVerificatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Tests\Service\Security;
+
+
+class UserMobileNumberVerificatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function verifyTest()
+    {
+        self::markTestIncomplete('Do this test');
+    }
+
+}
